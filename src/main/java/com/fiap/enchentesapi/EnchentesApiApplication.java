986b3ab9922a0ad04a59795f9c7810e4c7879f21
joaopardini <131975713+joaopardini@users.noreply.github.com>
2025-06-01 20:31:56 -0300

    Add files via upload

---
+++ src/main/java/com/fiap/enchentesapi/EnchentesApiApplication.java
@@ -0,0 +1,11 @@
+package com.fiap.enchentesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchentesApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnchentesApiApplication.class, args);
+    }
+}
